0f77cadc55a88c22df311164b3703206f9bea130
Roman <filiroman.tsu@gmail.com>
2013-12-16 02:35:42 +0400

    demo app

---
+++ PVMDCT_DEMO/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  PVMDCT_DEMO
+//
+//  Created by Roman Filippov on 10.12.13.
+//  Copyright Roman Filippov 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
